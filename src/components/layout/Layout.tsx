ce41e0088cfd150ba5ab8dc7111b1045018a8d69
Mustafa <mustafasultanov220@gmail.com>
2024-04-03 20:13:03 +0300

    update button

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -3,13 +3,13 @@ import Header from "./header/Header";
 import scss from "./Layout.module.scss";
 
 const Layout = () => {
-  return (
-    <div className={scss.Layout}>
-      <Header />
-      <main>main</main>
-      <Footer />
-    </div>
-  );
+	return (
+		<div className={scss.Layout}>
+			<Header />
+			<main>main</main>
+			<Footer />
+		</div>
+	);
 };
 
 export default Layout;
