956b6c1d0b5fe484012a444edc1af25e69b12bd2
mac <mac@macs-MacBook-Pro.local>
2024-04-03 04:47:09 -0700

    added customInput

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import CustomInput from "./components/UI/CustomInputLogin/CustomInput";
 import Layout from "./components/layout/Layout";
 
 const App = () => {
   return (
     <>
       <Layout />
+      <CustomInput />
     </>
   );
 };
--- src/components/UI/CustomInputLogin/CustomInput.tsx
+++ src/components/UI/CustomInputLogin/CustomInput.tsx
@@ -1,10 +1,10 @@
 const CustomInput = () => {
-  return (
-    <div>
-      <input type="text" />
-      <input type="text" />
-    </div>
-  );
+	return (
+		<div>
+			<input placeholder="rwrweq" type="text" />
+			<textarea placeholder="ygg"></textarea>
+		</div>
+	);
 };
 
 export default CustomInput;
