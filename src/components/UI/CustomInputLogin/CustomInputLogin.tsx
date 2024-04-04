0bdb34126bea80ab197b72e7e74b1d178d53fe75
Kasym <ulanbek.kasym@gmail.com>
2024-04-04 11:39:41 +0300

    added login inputs

---
+++ src/components/UI/CustomInputLogin/CustomInputLogin.tsx
@@ -0,0 +1,7 @@
+import styles from "./CustomInputLogin.module.scss";
+
+const CustomInput = () => {
+  return <input className={styles.input} placeholder="name" type="text" />;
+};
+
+export default CustomInput;
